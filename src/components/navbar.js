2521e7622a102bb8cfaf52cebc408fb7bca3251f
imhta <imhtapm@gmail.com>
2019-11-13 22:01:20 +0530

    changed the nav layout and background

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,30 +1,32 @@
 const generateTab = (name, selected) => {
-    const className = `class="${selected === name ? 'selected' : ''}" `
-    return `
+  const className = `class="${selected === name ? "selected" : ""}" `;
+  return `
     <li>
         <a ${className} data-role="tab" data-id="${name}" href="#!">
             ${name}
         </a>
-    </li>`
-}
+    </li>`;
+};
 
 const Navbar = (function() {
-    const nav = (selected) => `
+  const nav = selected => `
     <header>
         <nav>
             <a href="#!" class="brand-logo">foodbar</a>
+        </nav>
+        <nav>
             <ul>
-                ${generateTab('home', selected)}
-                ${generateTab('menu', selected)}
-                ${generateTab('contact', selected)}
+                ${generateTab("home", selected)}
+                ${generateTab("menu", selected)}
+                ${generateTab("contact", selected)}
             </ul>
         </nav>
-    </header>`
-    return {
-        render(selected) {
-            return nav(selected)
-        }
+    </header>`;
+  return {
+    render(selected) {
+      return nav(selected);
     }
-})()
+  };
+})();
 
-export default Navbar
+export default Navbar;
