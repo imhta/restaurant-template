02a7eb0422dd52b68131ec80a2ecc0a3c8684fe3
imhta <imhtapm@gmail.com>
2019-11-14 16:09:49 +0530

    contact page style and content added

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,15 @@ const ContactPage = () => {
     <main>
         <h1>Contact</h1>
         <section class="card">
-            
+        <div class="contact">
+            <p>For orders call us on 1800 1990 4555</p>
+            <div class="mapouter">
+                <div class="gmap_canvas">
+                    <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=foodbar&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                    <a href="https://www.embedgooglemap.net/blog/purevpn-coupon/">purevpn kodi addon</a>
+                </div>
+            </div>
+        </div>
         </section>
     </main>
     `
