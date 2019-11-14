c79ca9177126bf6c28d98551f1c55a8bf5524c71
imhta <imhtapm@gmail.com>
2019-11-14 13:42:06 +0530

    added destruction of event object

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import "./scss/main.scss";
 import Page from "./pages/page";
 
 
-document.addEventListener('click', (e) => {
-    const { target } = e;
+document.addEventListener('click', ({target}) => {
     if (target.getAttribute('data-role') === 'tab') {
       const tabName = target.getAttribute('data-id');
       Page.render(tabName);
