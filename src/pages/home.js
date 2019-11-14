2f21d6f86eab3478f8ad24ca0c4f624cd1e4e029
imhta <imhtapm@gmail.com>
2019-11-14 14:30:09 +0530

    home page ui completed

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,12 @@
 const HomePage = () => {
-    return `
+  return `
     <main>
-        <h1>awesome food from your neighourhood</h1>
+        <h1>fresh food from your neighourhood</h1>
     </main>
-    `
-}
+    <footer>
+        <p><span>&copy;</span> All rights are reserved to <span class="brand-logo">foodbar</span> <span>inc</span></p> 
+    </footer>
+    `;
+};
 
-export default HomePage
+export default HomePage;
