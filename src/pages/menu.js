be80f4572f4089ffb72d035de6f824358ccca326
imhta <imhtapm@gmail.com>
2019-11-14 15:50:08 +0530

    menu page completed

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,12 +1,34 @@
+const generateFood = i => `
+    <article>
+        <div class="img-wrapper">
+            <img src="https://source.unsplash.com/200x200/?food,cooked,${i}">
+        </div>
+        <div class="content">
+            <p>foodbar code # ${i}</p>
+            <small class="text-primary">
+                ${Math.floor(Math.random() * 20) + 1} $
+            </small>
+        <div>
+    </article>
+`;
+const generateFoodCard = noOfCard => {
+  let cards = "";
+  for (let i = 1; i <= noOfCard; i++) {
+    cards += generateFood(i);
+  }
+  return `
+    <section class="card">
+        ${cards}
+    </section>
+  `;
+};
 const MenuPage = () => {
-    return `
+  return `
     <main>
         <h1>Menu</h1>
-        <section class="card">
-            
-        </section>
+        ${generateFoodCard(20)}
     </main>
-    `
-}
+    `;
+};
 
-export default MenuPage
+export default MenuPage;
