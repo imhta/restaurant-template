68cbe7fb164b726a49d66821ca497243d3b3c7ca
imhta <imhtapm@gmail.com>
2019-11-14 16:36:02 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
 /* eslint-disable no-undef */
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist")
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
       {
         test: /\.scss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"]
+        use: ['style-loader', 'css-loader', 'sass-loader']
       },
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"]
+            presets: ['@babel/preset-env']
           }
         }
       }
